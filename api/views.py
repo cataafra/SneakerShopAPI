b4e371c04995e383e078b2a3984ffafd056e97e9
cataafra <afr.catalin@gmail.com>
2023-04-03 18:04:08 +0300

    added bulk add functionality

--- api/views.py
+++ api/views.py
@@ -37,6 +37,14 @@ class SneakerList(generics.ListCreateAPIView):
 
         return queryset
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=True)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class SneakerDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SneakerSerializerDetailed
