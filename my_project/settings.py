d94845cccb2d04ec75ce366f2eec388ebbaa5c37
cataafra <afr.catalin@gmail.com>
2023-04-03 16:31:33 +0300

    edited setting for cors to work again again

--- my_project/settings.py
+++ my_project/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.CorsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
